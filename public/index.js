9e5a34dd92a1da8a7387ea2188cc62cba1955e06
OleksandrGlushchuk <glushchuk259@gmail.com>
2023-12-03 12:52:49 +0200

    first tutorial

--- public/index.js
+++ public/index.js
@@ -3,21 +3,39 @@ import * as THREE from "./thirdparty/threejs-math.module.js";
 
 console.log("Hello");
 
-while (true) {
+const canvas = document.querySelector("#glcanvas");
+//Initialize the GL context
+const gl = canvas.getContext("webgl");
 
-    const m = new THREE.Matrix4();
-    const m1 = new THREE.Matrix4();
-    const m2 = new THREE.Matrix4();
-    const m3 = new THREE.Matrix4();
-    const alpha = 0;
-    const beta = Math.PI;
-    const gamma = Math.PI/2;
-    m1.makeRotationX( alpha );
-    m2.makeRotationY( beta );
-    m3.makeRotationZ( gamma );
-    m.multiplyMatrices( m1, m2 );
-    m.multiply( m3 );
-
-    // This function should have some rendering code.
-    tutorial1.main();
+//Only continue if WebGL is available and working
+if (gl === null) {
+  alert(
+    "Unable to initialize WebGL. Your browser or machine may not support it."
+  );
 }
+else
+{
+  console.log("Successfully initialized WebGL context ");
+}
+
+tutorial1.main(gl);
+
+// while (true) {
+
+//     const m = new THREE.Matrix4();
+//     const m1 = new THREE.Matrix4();
+//     const m2 = new THREE.Matrix4();
+//     const m3 = new THREE.Matrix4();
+//     const alpha = 0;
+//     const beta = Math.PI;
+//     const gamma = Math.PI/2;
+//     m1.makeRotationX( alpha );
+//     m2.makeRotationY( beta );
+//     m3.makeRotationZ( gamma );
+//     m.multiplyMatrices( m1, m2 );
+//     m.multiply( m3 );
+
+//     // This function should have some rendering code.
+    
+//     //
+// }
--- public/tutorials/tutorial1/main.js
+++ public/tutorials/tutorial1/main.js
@@ -1,8 +1,110 @@
 import * as THREE from "./../../thirdparty/threejs-math.module.js";
+const vsSource = `
+  attribute vec3 aPos;
+  void main() {
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+  }
+`;
 
-export function main() {
+const fsSource = `
+  void main() {
+    gl_FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+  }
+`;
 
-    const m = new THREE.Matrix4();
+export function main(inGL) {
+    const gl = inGL;
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(vertexShader, vsSource);
+    gl.shaderSource(fragmentShader, fsSource);
+    gl.compileShader(vertexShader);
+    gl.compileShader(fragmentShader);
 
-    console.log("This should have some rendering code")
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+        alert(
+            `An error occurred compiling the shaders: ${gl.getShaderInfoLog(vertexShader)}`
+        );
+        gl.deleteShader(vertexShader);
+    }
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+        alert(
+            `An error occurred compiling the shaders: ${gl.getShaderInfoLog(fragmentShader)}`
+        );
+        gl.deleteShader(fragmentShader);
+    }
+
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    // If creating the shader program failed, alert
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert(
+            `Unable to initialize the shader program: ${gl.getProgramInfoLog(
+                shaderProgram
+            )}`
+        );
+    }
+
+    const vertices = [
+        0.5, 0.5, 0.0,  // top right
+        0.5, -0.5, 0.0,  // bottom right
+        -0.5, -0.5, 0.0,  // bottom left
+        -0.5, 0.5, 0.0   // top left 
+    ];
+    const indices = [  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    ];
+
+    // Get the Vertex Array Object extension and create/bind a VAO  
+    const ext = gl.getExtension("OES_vertex_array_object"); // Vendor prefixes may apply!  
+    if(ext === null)
+    {
+        alert(
+            "OES_vertex_array_object ext is not supported"
+          );
+    }
+    else
+    {
+        console.log("Successfully loaded OES_vertex_array_object extension")
+    }
+    const VAO = ext.createVertexArrayOES();
+
+    // Start setting up VAO  
+    ext.bindVertexArrayOES(VAO);  
+
+    const VBO = gl.createBuffer();
+    const EBO = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 12, 0);
+    gl.enableVertexAttribArray(0);
+
+    // note that this is allowed, the call to gl.vertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    // Finised setting up VAO  
+    ext.bindVertexArrayOES(null);  
+
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.2, 0.3, 0.3, 1.0);
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.useProgram(shaderProgram);
+    ext.bindVertexArrayOES(VAO);
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+    ext.bindVertexArrayOES(null);  
 }
